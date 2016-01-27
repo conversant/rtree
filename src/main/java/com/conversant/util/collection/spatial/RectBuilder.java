c4907942ce0cb3ddd0fdf9c1da39830f123472bb
James Covert <covert.james@gmail.com>
2016-01-27 15:57:07 -0600

    Added comments to RectBuilder interface

--- src/main/java/com/conversant/util/collection/spatial/RectBuilder.java
+++ src/main/java/com/conversant/util/collection/spatial/RectBuilder.java
@@ -4,7 +4,24 @@ package com.conversant.util.collection.spatial;
  * Created by jcairns on 4/30/15.
  */
 public interface RectBuilder<T> {
+
+    /**
+     * Build a bounding rectangle for the given element
+     *
+     * @param t - element to bound
+     *
+     * @return HyperRect impl for this entry
+     */
     HyperRect getBBox(T t);
 
+
+    /**
+     * Build a bounding rectangle for given points (min and max, usually)
+     *
+     * @param p1 - first point (top-left point, for example)
+     * @param p2 - second point (bottom-right point, for example)
+     *
+     * @return HyperRect impl defined by two points
+     */
     HyperRect getMbr(HyperPoint p1, HyperPoint p2);
 }
